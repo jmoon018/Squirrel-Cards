36ce5ba764ade59bd1dd93a5aadbc9bf99870783
Peter Haugen <Phaugen@Gmail.com>
2015-02-05 16:31:40 -0800

    added a seeding method, woo, lazy

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -5,4 +5,16 @@ describe 'User deck relation' do
     expect(User.public_instance_methods.include?(:decks)).to eq(true)
   end
 
+  it 'users can create decks' do
+    new_deck = {description: "drinking lots of juice", name: "Beverages"}
+    temp_user = User.last
+    expect{temp_user.decks.create(new_deck)}.to change{temp_user.decks.count}
+  end
+
+  it 'users can create decks' do
+    new_deck = {description: "drinking lots of juice", name: "Beverages"}
+    temp_user = User.last
+    temp_user.decks.create(new_deck)
+    expect(temp_user.decks.last.user_id).to eq(temp_user.id)
+  end
 end
